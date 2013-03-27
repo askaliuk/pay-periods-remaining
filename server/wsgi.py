c04d8d366eedad454a8e6bedc272c3e9b0b8fa16
Andrii Skaliuk <askaliuk@gmail.com>
2013-03-27 16:14:15 -0700

    dotcloud deployment

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,9 +1,2 @@
-# from app import app
-# application = app.wsgifunc()
-
-def application(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    return ['Hello world!\n'] + ['{0}={1}\n'.format(k,environ[k])
-                                 for k in sorted(environ)]
+from app import app
+application = app.wsgifunc()
